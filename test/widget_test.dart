c5f6523fd5c1725a9310fc59cb0b6e155eea00ee
Pranav Dwivedi <pranavdwivedi@Pranavs-MacBook-Pro.local>
2026-01-06 22:00:05 +1100

    Fix flutter_rust_bridge configuration to point to native/ directory and remove scaffolded rust/ dir

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gba_forge/main.dart';
-import 'package:gba_forge/src/features/editor/screens/home_screen.dart';
 
 // Helper to wrap widgets in ProviderScope
 Widget createWidgetUnderTest() {
